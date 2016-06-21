0b4e5d231b6d3e7a830855639b1590f80f3d44ff
ebondrake <ebon.drake@outlook.com>
2016-06-21 12:46:48 -0600

    Create role.attacker.js

---
+++ role.attacker.js
@@ -0,0 +1,16 @@
+var roleAttacker = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+    
+        var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(target){
+            if(creep.attack(target) == ERR_NOT_IN_RANGE){
+                creep.moveTo(target);
+                console.log('Attacking: ' + target);
+            }
+        }
+	}
+};
+
+module.exports = roleAttacker;
