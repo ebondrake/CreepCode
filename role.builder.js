4490aed4191ad91e74ba4a4af5a039f884687cb1
ebondrake <ebon.drake@outlook.com>
2016-06-22 16:16:17 -0600

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -12,35 +12,36 @@ var roleBuilder = {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
+            console.log('building false');
 	    }
 	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
+          console.log('building true');
 	    }
 
 
 	    if(!creep.memory.building) {
+        console.log(creep.name + ' is not building');
             if(creep.carry.energy < creep.carryCapacity){
+              console.log(creep.name + ' is harvesting');
                 if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(source);
                 }
             }else {
+              console.log(creep.name + ' is not building, is full, and is going to wait by flag.');
                 creep.moveTo(Game.flags.Builder);
             }
 	    }else {//if creep is building
+            console.log(creep.name + ' is building!');
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
+                console.log(creep.name + ' is buildinga new construction site: ' + target.name);
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }//if target not in range
             }else{//if no target
-
+              console.log(creep.name + ' is looking for a structure to build to 1/3rd');
                 //find items to finish building
-
-                var spawnToBuild = creep.room.find(FIND_STRUCTURES,
-                  filter: function(object){
-                    return object.structureType === STRUCTURE_SPAWN && (object.hits <= object.hitsMax / 3);
-                }
-            });
 
                 var spawnToBuild = creep.room.find(FIND_STRUCTURES, {
                     filter: function(object){
@@ -93,12 +94,18 @@ var roleBuilder = {
                 }
 
                 if (itemToBuild > 0){
+                                console.log(creep.name + ' has an item to build and is sorting them.');
                   itemToBuild.sort((a,b) => a.hits - b.hits);
+                                console.log(creep.name + ' has sorted items to build and is going to build ' + itemToBuild[0]);
                     //console.log(itemToBuild);
                   if(creep.repair(itemToBuild[0]) == ERR_NOT_IN_RANGE){
                     creep.moveTo(itemToBuild[0]);
-                  }
-                }//if no item to build
+                  }//no item to build
+                }else {
+                  console.log(creep.name + ' is building but has no items to build so waiting at flag.');
+                  creep.moveTo(Game.flags.Builder);
+                }
+
             }//if no target
       }//if building
 
