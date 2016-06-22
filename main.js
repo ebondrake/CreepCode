40c9947341e022db166f7cb14b415efa2c2efc6b
ebondrake <ebon.drake@outlook.com>
2016-06-22 11:05:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,55 +1,31 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 var roleAttacker = require('role.attacker');
-
+var roleRepairer = require('role.repairer');
+var cleanup = require('cleanup');
+var spawnAI = require('spawnAI')
 
 module.exports.loop = function () {
-    
-	var harvestersTotal = 6;
-	var buildersTotal = 4;
-	var upgradersTotal = 4;
-	var attackersTotal = 4;
+  cleanup.run(cleanup);
+  spawnAI.run(spawn);
 
-	var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-	var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-	var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-	var attackers = _.filter(Game.creeps, (creep) => creep.memory.role == 'attacker');
-	
-	for(var name in Memory.creeps) {
-		if(!Game.creeps[name]) {
-			delete Memory.creeps[name];
-		}
-	}
-	
-	if(harvesters.length < harvestersTotal && (Game.rooms.W29S18.energyAvailable > 200)){
-		Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-	}else{
-		if(builders.length < buildersTotal && (Game.rooms.W29S18.energyAvailable > 200)){
-			Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
-		}else{
-			if(upgraders.length < upgradersTotal && (Game.rooms.W29S18.energyAvailable > 200)){
-				Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
-			}else{
-    				if(attackers.length < attackersTotal && (Game.rooms.W29S18.energyAvailable > 400)){
-	    				Game.spawns.Spawn1.createCreep([ATTACK,ATTACK,MOVE], undefined, {role: 'attacker'});
-				}
-			}
-		}
-	}
-
-	for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-        if(creep.memory.role == 'attacker') {
-            roleAttacker.run(creep);
-        }
+  for(var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if(creep.memory.role == 'harvester') {
+      roleHarvester.run(creep);
     }
+    if(creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+    if(creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
+    }
+    if(creep.memory.role == 'attacker') {
+      roleAttacker.run(creep);
+    }
+    if(creep.memory.role == 'repairer') {
+      roleRepairer.run(creep);
+    }
+  }
 }
