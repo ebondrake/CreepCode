d9a7bd88968559c3964c24c29f31c994fa07d6f8
ebondrake <ebon.drake@outlook.com>
2016-06-22 15:29:52 -0600

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,5 +1,5 @@
 var roleBuilder = {
-    
+
 
     /** @param {Creep} creep **/
     run: function(creep) {
@@ -7,8 +7,8 @@ var roleBuilder = {
         //source ids
         var source1 = Game.getObjectById('55db3344efa8e3fe66e05704');
         var source2 = Game.getObjectById ('55db3344efa8e3fe66e05705');
-        
         var source = source1;
+        var itemToBuild;
 
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
@@ -19,22 +19,81 @@ var roleBuilder = {
 	    }
 
 
-	    if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+	    if(!creep.memory.building) {
+            if(creep.carry.energy < creep.carryCapacity){
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }else {
+                creep.moveTo(Game.flags.Builder);
+            }
+	    }else {//if creep is building
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
-                }
-            }else{
-             creep.moveTo(Game.flags.Builder);   
-            }
-	    }
-	    else {
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            }
-	    }
-	}
-};
+                }//if target not in range
+            }else{//if no target
+                
+                //find items to finish building
+                var spawnToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: function(object){
+                        return object.structureType === STRUCTURE_SPAWN && (object.hits <= object.hitsMax / 3);
+                    }
+                });
 
-module.exports = roleBuilder;
+                var extensionToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_EXTENSION && (object.hits <= object.hitsMax / 3);
+                  }
+                });
+        
+                var rampartToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_RAMPART && (object.hits <= object.hitsMax / 3);
+                  }
+                });
+        
+                var wallToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_WALL && (object.hits <= object.hitsMax / 3);
+                  }
+                });
+        
+                var roadToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_ROAD && (object.hits <= object.hitsMax / 3);
+                  }
+                });
+        
+                if (spawnToBuild){
+                  itemToBuild = spawnToBuild;
+                }
+        
+                else if (extensionToBuild) {
+                  itemToBuild = extentionToRepair;
+                }
+        
+                else if (rampartToBuild) {
+                  itemToBuild = rampartToBuild;
+                }
+        
+                else if (wallToBuild) {
+                  itemToBuild = wallToBuild;
+                }
+        
+                else if (roadToBuild) {
+                  itemToBuild = roadToBuild;
+                }
+        
+                if (itemToBuild){
+                    //console.log(itemToBuild);
+                  if(creep.repair(itemToBuild) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(itemToBuild);
+                  }
+                }//if no item to build
+            }//if no target
+      }//if building
+      
+    }//end of run function
+  };module.exports = roleBuilder;
