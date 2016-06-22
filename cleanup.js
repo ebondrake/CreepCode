8e9e72fe6ad0018f772d9ecc818ec06a9abb82ee
ebondrake <ebon.drake@outlook.com>
2016-06-22 13:54:52 -0600

    Create cleanup.js

---
+++ cleanup.js
@@ -0,0 +1,11 @@
+var cleanup = {
+
+  run: function(cleanup) {
+    for(var name in Memory.creeps) {
+      if(!Game.creeps[name]) {
+        delete Memory.creeps[name];
+      }
+    }
+  }
+
+};module.exports = cleanup;
