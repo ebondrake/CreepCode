2a7e31d67ec042176d74644888fbd4c1df112723
ebondrake <ebon.drake@outlook.com>
2016-06-22 13:54:23 -0600

    Update role.attacker.js

--- role.attacker.js
+++ role.attacker.js
@@ -1,18 +1,19 @@
 var roleAttacker = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-    
-        var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(target){
-            if(creep.attack(target) == ERR_NOT_IN_RANGE){
-                creep.moveTo(target);
-                console.log('Attacking: ' + target);
-            }
-        }else{
-            creep.moveTo(Game.flags.Attacker);
-        }
-	}
-};
+  run: function(creep) {
 
-module.exports = roleAttacker;
+    var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+    if(target){
+      if(creep.attack(target) == ERR_NOT_IN_RANGE){
+        creep.moveTo(target);
+      }else {
+        console.log('Attacking: ' + target + '!');
+      }
+    }else {
+      creep.moveTo(Game.flags.Attacker);
+    }
+
+  }//end run function
+
+};module.exports = roleAttacker;
