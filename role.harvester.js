3e118c70e788104674b5048b68d29c1d074f9838
ebondrake <ebon.drake@outlook.com>
2016-06-22 11:12:10 -0600

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,19 +1,36 @@
 var roleHarvester = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var source = creep.pos.findClosestByRange(FIND_SOURCES);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            }
-        }
-        else {
-            if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns.Spawn1);
-            }
-        }
-	}
-};
+  run: function(creep) {
+    //source ids
+    var source1 = Game.getObjectById('55db3344efa8e3fe66e05704');
+    var source2 = Game.getObjectById ('55db3344efa8e3fe66e05705');
+    var source = source1;
 
-module.exports = roleHarvester;
+    if(creep.carry.energy < creep.carryCapacity) {
+      if(creep.harvest(source1) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source1);
+      }
+
+      if(creep.harvest(source1) == ERR_NO_PATH) {
+        if(creep.harvest(source2) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source2);
+        }
+      }
+
+    }else {//if creep carry capacity full
+
+      //find empty energy containers
+      var targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;}});
+
+      if(targets.length > 0) {
+        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0]);
+        }
+      }else {//energy full and no targets to fill
+        creep.moveTo(Game.flags.Harvester);
+      }
+    }
+
+  }//end run function
+
+};module.exports = roleHarvester;
