7dcc875990667d147f29da3da613c38ee361ba7a
ebondrake <ebon.drake@outlook.com>
2016-06-22 13:52:43 -0600

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,39 +1,31 @@
 var roleUpgrader = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        
-                //source ids
-        var source1 = Game.getObjectById('55db3344efa8e3fe66e05704');
-        var source2 = Game.getObjectById ('55db3344efa8e3fe66e05705');
-        var source = source2;
-        
-//        if(creep.room.name !== Game.spawns.Spawn1.roomName) {
-//            creep.moveTo(Game.spawns.Spawn1);
-//        }
-//        else {
-            if(creep.memory.upgrading && creep.carry.energy == 0) {
-               creep.memory.upgrading = false;
-	        }
-	        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-	            creep.memory.upgrading = true;
-	        }
-        
-	        if(!creep.memory.upgrading) {
-                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(source);
-                }
-            }
-            else {
-                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
-                }
-            }
-        //}
+  run: function(creep) {
 
-    }
-};
+  var source1 = Game.getObjectById('55db3344efa8e3fe66e05704');
+  var source2 = Game.getObjectById ('55db3344efa8e3fe66e05705');
+  var source = source2;
 
+  //set creep upgrading to false
+  if(creep.memory.upgrading && creep.carry.energy == 0) {
+    creep.memory.upgrading = false;
+  }
 
-module.exports = roleUpgrader;
- 
+  //set upgrading to true
+  if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+	   creep.memory.upgrading = true;
+   }
+
+   if(!creep.memory.upgrading) {
+     if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+       creep.moveTo(source);
+     }
+   }else {
+     if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+       creep.moveTo(creep.room.controller);
+     }
+   }
+
+ }//end run function
+
+};module.exports = roleUpgrader;
