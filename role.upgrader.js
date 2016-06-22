ca47625bba843d8c400609cb66b5f70cae17cfbb
ebondrake <ebon.drake@outlook.com>
2016-06-22 11:13:07 -0600

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,19 +1,31 @@
 var roleUpgrader = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.carry.energy == 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        }
-	}
-}
+  run: function(creep) {
 
-;module.exports = roleUpgrader;
+  var source1 = Game.getObjectById('55db3344efa8e3fe66e05704');
+  var source2 = Game.getObjectById ('55db3344efa8e3fe66e05705');
+  var source = source2;
+
+  //set creep upgrading to false
+  if(creep.memory.upgrading && creep.carry.energy == 0) {
+    creep.memory.upgrading = false;
+  }
+
+  //set upgrading to true
+  if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+	   creep.memory.upgrading = true;
+   }
+
+   if(!creep.memory.upgrading) {
+     if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+       creep.moveTo(source);
+     }
+   }else {
+     if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+       creep.moveTo(creep.room.controller);
+     }
+   }
+
+ }//end run function
+
+};module.exports = roleUpgrader;
