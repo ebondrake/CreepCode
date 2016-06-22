eb49c03c7e706e113cb0a43363ede364249556d2
ebondrake <ebon.drake@outlook.com>
2016-06-22 15:43:51 -0600

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,7 +1,6 @@
 var roleBuilder = {
 
 
-    /** @param {Creep} creep **/
     run: function(creep) {
 
         //source ids
@@ -20,81 +19,88 @@ var roleBuilder = {
 
 
 	    if(!creep.memory.building) {
-        if(creep.carry.energy < creep.carryCapacity){
-          if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
-          }
-        }else {
-          creep.moveTo(Game.flags.Builder);
-        }
-      }else {//if creep is building
+            if(creep.carry.energy < creep.carryCapacity){
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }else {
+                creep.moveTo(Game.flags.Builder);
+            }
+	    }else {//if creep is building
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }//if target not in range
+            }else{//if no target
 
-        //find items to repair
-        var spawnToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: function(object){
-            return object.structureType === STRUCTURE_SPAWN && (object.hits < object.hitsMax / 3);
-          }
-        });
+                //find items to finish building
 
-        var extensionToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: function(object){
-            return object.structureType === STRUCTURE_EXTENSION && (object.hits < object.hitsMax / 3);
-          }
-        });
+                var spawnToBuild = creep.room.find(FIND_STRUCTURES,
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_SPAWN && (object.hits <= object.hitsMax / 3);
+                }
+            });
 
-        var rampartToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: function(object){
-            return object.structureType === STRUCTURE_RAMPART && (object.hits < object.hitsMax / 3);
-          }
-        });
+                var spawnToBuild = creep.room.find(FIND_STRUCTURES, {
+                    filter: function(object){
+                        return object.structureType === STRUCTURE_SPAWN && (object.hits <= object.hitsMax / 3);
+                    }
+                });
 
-        var wallToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: function(object){
-            return object.structureType === STRUCTURE_WALL && (object.hits < object.hitsMax / 3);
-          }
-        });
+                var extensionToBuild = creep.room.find(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_EXTENSION && (object.hits <= object.hitsMax / 3);
+                  }
+                });
 
-        var roadToBuild = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: function(object){
-            return object.structureType === STRUCTURE_ROAD && (object.hits < object.hitsMax / 3);
-          }
-        });
+                var rampartToBuild = creep.room.find(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_RAMPART && (object.hits <= object.hitsMax / 3);
+                  }
+                });
 
-        if (spawnToBuild){
-          itemToBuild = spawnToBuild;
-        }
+                var wallToBuild = creep.room.find(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_WALL && (object.hits <= object.hitsMax / 3);
+                  }
+                });
 
-        else if (extensionToBuild) {
-          itemToBuild = extentionToRepair;
-        }
+                var roadToBuild = creep.room.find(FIND_STRUCTURES, {
+                  filter: function(object){
+                    return object.structureType === STRUCTURE_ROAD && (object.hits <= object.hitsMax / 3);
+                  }
+                });
 
-        else if (rampartToBuild) {
-          itemToBuild = rampartToBuild;
-        }
+                if (spawnToBuild){
+                  itemToBuild = spawnToBuild;
+                }
 
-        else if (wallToBuild) {
-          itemToBuild = wallToBuild;
-        }
+                else if (extensionToBuild) {
+                  itemToBuild = extentionToRepair;
+                }
 
-        else if (roadToBuild) {
-          itemToBuild = roadToBuild;
-        }
+                else if (rampartToBuild) {
+                  itemToBuild = rampartToBuild;
+                }
 
-        if (itemToBuild){
-            //console.log(itemToBuild);
-          if(creep.repair(itemToBuild) == ERR_NOT_IN_RANGE){
-            creep.moveTo(itemToBuild);
-          }
-        }else {
-          var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-          if(targets.length) {
-            if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(targets[0]);
-            }//if target not in range
-          }//if no target
-        }//if no item to build
+                else if (wallToBuild) {
+                  itemToBuild = wallToBuild;
+                }
 
+                else if (roadToBuild) {
+                  itemToBuild = roadToBuild;
+                }
+
+                if (itemToBuild > 0){
+                  itemToBuild.sort((a,b) => a.hits - b.hits);
+                    //console.log(itemToBuild);
+                  if(creep.repair(itemToBuild[0]) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(itemToBuild[0]);
+                  }
+                }//if no item to build
+            }//if no target
       }//if building
-      
+
     }//end of run function
   };module.exports = roleBuilder;
