377d230632e7edb109712c16d10dd006c0dc659d
ebondrake <ebon.drake@outlook.com>
2016-06-22 08:29:37 -0600

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,7 +1,15 @@
 var roleBuilder = {
+    
 
     /** @param {Creep} creep **/
     run: function(creep) {
+
+        //source ids
+        var source1 = Game.getObjectById('55db3344efa8e3fe66e05704');
+        var source2 = Game.getObjectById ('55db3344efa8e3fe66e05705');
+        
+        var source = source1;
+
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
@@ -16,10 +24,11 @@ var roleBuilder = {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
+            }else{
+             creep.moveTo(Game.flags.Builder);   
             }
 	    }
 	    else {
-	        var source = creep.pos.findClosestByRange(FIND_SOURCES);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
