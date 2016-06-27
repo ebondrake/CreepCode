d1ded405ecc10287dde6a1cfb0e5f0e5f16242f8
ebondrake <ebon.drake@outlook.com>
2016-06-27 14:19:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,13 @@
 var cleanup = require('cleanup');
-var AIspawn = require('AIspawn');
 var AIrun = require('AIrun');
+var AIspawn = require('AIspawn');
+var roomDefense = require('roomDefense');
 
 module.exports.loop = function () {
-
-  //console.log('broken');
 
   cleanup.run(cleanup);
   AIrun.run(AIrun);
   AIspawn.run(AIspawn);
-
-  var tower = Game.getObjectById('576d64ee4c28d01755d99541');
-
-if(tower){
-  var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-  if(closestHostile) {
-      tower.attack(closestHostile);
-  }
-}
-
-if(tower && (Memory.harvester.length == Memory.harvesterTotal) && (Memory.builder.length == Memory.builderTotal) && Memory.roomFull) {
-
-    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: (structure) => structure.hits < structure.hitsMax
-    });
-    if(closestDamagedStructure) {
-        tower.repair(closestDamagedStructure);
-    }
-}
+  roomDefense.run(roomDefense);
 
 }//END loop function
