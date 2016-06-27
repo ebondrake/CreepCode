c16457715b890580b26185256ee8b96386f9268e
ebondrake <ebon.drake@outlook.com>
2016-06-27 14:22:10 -0600

    Create roomDefense.js

---
+++ roomDefense.js
@@ -0,0 +1,21 @@
+var roomDefense = {
+  run: function(creep) {
+    var tower = Game.getObjectById('576d64ee4c28d01755d99541');
+    if(tower){
+      var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+      if(closestHostile) {
+        tower.attack(closestHostile);
+      }
+    }
+
+    if(tower && (Memory.harvester.length == Memory.harvesterTotal) && (Memory.builder.length == Memory.builderTotal) && Memory.roomFull) {
+      var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => structure.hits < structure.hitsMax
+      });
+
+      if(closestDamagedStructure) {
+        tower.repair(closestDamagedStructure);
+      }
+    }
+  }
+};module.exports = roomDefense;
