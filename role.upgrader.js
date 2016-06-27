0915dad52059941fa142c956ee37cc47c557f1df
ebondrake <ebon.drake@outlook.com>
2016-06-27 13:10:40 -0600

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,29 +2,39 @@ var roleUpgrader = {
 
   run: function(creep) {
 
-    var source = creep.pos.findClosestByPath(FIND_SOURCES);
-    creep.memory.source = source;
+    if(creep.memory.upgrading === null){
+      creep.memory.upgrading = true;
+    }
 
-  //set creep upgrading to false
-  if(creep.memory.upgrading && creep.carry.energy == 0) {
-    creep.memory.upgrading = false;
-  }
-
-  //set upgrading to true
-  if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+    if(creep.memory.upgrading && creep.carry.energy == 0) {
+      creep.memory.upgrading = false;
+    }
+    else if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
 	   creep.memory.upgrading = true;
    }
 
    if(!creep.memory.upgrading) {
-     if(creep.harvest(creep.memory.source) == ERR_NOT_IN_RANGE) {
-       creep.moveTo(creep.memory.source);
+
+     var extentionSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy === structure.energyCapacity;}});
+     var spawnSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_SPAWN) && structure.energy === structure.energyCapacity;}});
+
+     if(extentionSource){
+       var energySource = extentionSource;
+     }else if (spawnSource) {
+       var energySource = spawnSource;
+     }
+
+     if(energySource && Memory.harvesterFull && (Memory.roomEnergy >= Memory.roomEnergyCapacity / 3)){
+       if(energySource.transferEnergy(creep) == ERR_NOT_IN_RANGE){
+         creep.moveTo(energySource);
+       }
+     }else {
+      creep.moveTo(Game.flags.idle);
      }
    }else {
      if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
        creep.moveTo(creep.room.controller);
      }
    }
-
- }//end run function
-
+ }
 };module.exports = roleUpgrader;
