7a1f7beed81efff7bdafd15f1c1c46451a5fbd87
ebondrake <ebon.drake@outlook.com>
2016-07-12 10:21:57 -0600

    Update AIspawn.js

--- AIspawn.js
+++ AIspawn.js
@@ -1,34 +1,29 @@
-var AIspawn = {
+var AIspawnLvl1 = require('AIspawnLvl1');
+var AIspawnLvl2 = require('AIspawnLvl2');
+var AIspawnLvl3 = require('AIspawnLvl3');
+var AIspawnLvl4 = require('AIspawnLvl4');
+var AIspawnLvl5 = require('AIspawnLvl5');
 
-  run: function(AIspawn) {
-
-
-    var AIspawnLvl1 = require('AIspawnLvl1');
-    var AIspawnLvl2 = require('AIspawnLvl2');
-
-
-    if(Memory.cityLevel === 1 && Memory.roomEnergyFull){
-
-      AIspawnLvl1.run(AIspawnLvl1);
+module.exports={
+  run: function(){
+    if (Memory.cityLevel === 1 && Memory.roomEnergy >= 300){
+      AIspawnLvl1.run();
     }
 
-    else if(Memory.cityLevel === 2 && Memory.roomEnergyFull){
-      if(Memory.harvester.length > 1){
-        AIspawnLvl2.run(AIspawnLvl2);
-      }else {
-        AIspawnLvl1.run(AIspawnLvl1);
-      }
+    else if(Memory.cityLevel === 2 && Memory.roomEnergy >= 550){
+        AIspawnLvl2.run();
     }
 
-    else if(Memory.cityLevel === 3 && Memory.roomEnergy >= 550){
-
-      if(Memory.harvester.length > 1){
-        AIspawnLvl2.run(AIspawnLvl2);
-      }else {
-        AIspawnLvl1.run(AIspawnLvl1);
-      }
+    else if(Memory.cityLevel === 3 && Memory.roomEnergy >= 800){
+        AIspawnLvl3.run();
     }
 
+    else if(Memory.cityLevel === 4 && Memory.roomEnergy >= 1300){
+        AIspawnLvl4.run();
+    }
 
+    else if(Memory.cityLevel === 5 && Memory.roomEnergy >= 1800){
+        AIspawnLvl5.run();
+    }
   }
-};module.exports = AIspawn;
+}
