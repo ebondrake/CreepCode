fd7a4a3b75af09332240a1d47e80ccf02f3434f5
ebondrake <ebon.drake@outlook.com>
2016-07-12 10:34:23 -0600

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,85 +1,100 @@
-var roleBuilder = {
+var roleCollector = require('role.collector');
+var roleUpgrader = require('role.upgrader');
 
-  run: function(creep) {
+module.exports={
+  run: function(creep){
 
-    var buildCap = 500;
+//    if(Memory.test === undefined || Memory.test >= 100){Memory.test = 0};
+//    Memory.test++;
+    //console.log(creep.room.name);
 
-    if(creep.memory.building === null){
-      creep.memory.building = false;
-    }
+    var energySource;
+    if(Memory.builder.length > Memory.builderTotal || (creep.memory.level < Memory.cityLevel && Memory.builder.length > 1)){
+      creep.drop(RESOURCE_ENERGY);
+      creep.moveTo(Game.spawns.Home);
+    }else{
+      if(creep.ticksToLive >= 1450 || Game.spawns.Home.energy < 20) {creep.memory.aboutToDie = false}
 
-    if(creep.memory.building && creep.carry.energy === 0) {
-      creep.memory.building = false;
-    }
+      if(creep.memory.working && creep.carry.energy === 0) {creep.memory.working = false;}
+      if(!creep.memory.working && creep.carry.energy === creep.carryCapacity)  {creep.memory.working = true;}
 
-    if(!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
-	     creep.memory.building = true;
-     }
+      if(creep.memory.aboutToDie){
+        creep.drop(RESOURCE_ENERGY);
+        creep.moveTo(Game.spawns.Home);
+      }else{
+        if(!creep.memory.working){
+          if(Memory.roomNeedsRepaired){
+            var storageSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_STORAGE) && structure.store[RESOURCE_ENERGY] > 0;}});
+            var containerSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0;}});
+            var extentionSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy > 0;}});
+            var spawnSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_SPAWN) && structure.energy === 300;}});
 
-     //creep.say(creep.memory.building);
+            if(storageSource) {energySource = storageSource;}
+            else if(containerSource)  {energySource = containerSource;}
+            else if(extentionSource)  {energySource = extentionSource;}
+            else if(spawnSource)  {energySource = spawnSource;}
 
-     if(!creep.memory.building) {
-       //creep.say(creep.carry.energy < creep.carryCapacity);
-       if(creep.carry.energy < creep.carryCapacity){
-         var extentionSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy === structure.energyCapacity;}});
-         var spawnSource = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_SPAWN) && structure.energy === structure.energyCapacity;}});
+            if(energySource && Memory.roomEnergy >= Memory.roomEnergyCapacity / 2){
+              if(energySource.structureType === STRUCTURE_CONTAINER || energySource.structureType === STRUCTURE_STORAGE){
+                if(energySource.transfer(creep, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE){
+                  creep.moveTo(energySource);
+                }
+              }else{
+                if(Memory.creepFull){
+                  if(creep.withdraw(energySource, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE){
+                    creep.moveTo(energySource);
+                  }
+                }else{ //END if energy source
+                  //creep.moveTo(Memory.Idle);
+                  roleUpgrader.run(creep);
+                }
+              }
+            }else{ //END if energy source
+              //creep.moveTo(Memory.Idle);
+              roleUpgrader.run(creep);
+            }
+          }else{
+            //creep.moveTo(Memory.Idle);
+            roleUpgrader.run(creep);
+          }
+            }else{//find items to finish building
+                var targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_SPAWN ) && (structure.hits < structure.hitsMax);}});
+                if (!targets.length){
+                    targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_EXTENSION ) && (structure.hits  < structure.hitsMax);}});
+                    if (!targets.length) {
+                        targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_RAMPART ) && (structure.hits  < structure.hitsMax/28);}});
+                        if (!targets.length) {
+                            targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_CONTAINER ) && (structure.hits  < structure.hitsMax);}});
+                            if (!targets.length) {
+                                targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_STORAGE ) && (structure.hits  < structure.hitsMax);}});
+                                if (!targets.length) {
+                                  targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_WALL ) && (structure.hits  < structure.hitsMax/1000);}});
+                                  if (!targets.length) {
+                                    targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_ROAD ) && (structure.hits  < structure.hitsMax/2);}});
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
 
-         if(extentionSource){
-           var energySource = extentionSource;
-         }else if (spawnSource) {
-           var energySource = spawnSource;
-         }
-         //console.log(energySource + ' - ' + Memory.harvesterFull + ' - ' + Memory.roomEnergy + ' - ' + Memory.roomEnergyCapacity / 2);
-         //console.log(energySource && Memory.harvesterFull && (Memory.roomEnergy >= Memory.roomEnergyCapacity / 2));
-         if(energySource && Memory.harvesterFull && (Memory.roomEnergy >= Memory.roomEnergyCapacity / 3)){
-           if(energySource.transferEnergy(creep) === ERR_NOT_IN_RANGE){
-             creep.moveTo(energySource);
-           }
-         }else {
-           //creep.say('Idle');
-           creep.moveTo(Game.flags.Idle);
-         }
-       }
-     }else {//if creep is building
-
-       //find items to finish building
-       var targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_SPAWN ) && (structure.hits <= buildCap);}});
-       if (!targets.length){
-         targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_EXTENSION ) && (structure.hits <= buildCap);}});
-         if (!targets.length) {
-           targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_RAMPART ) && (structure.hits <= buildCap);}});
-           if (!targets.length) {
-             targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_WALL ) && (structure.hits <= buildCap);}});
-             if (!targets.length) {
-               targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_ROAD ) && (structure.hits <= buildCap);}});
-             }
-           }
-         }
-       }
-
-
-       if(targets.length) {
-         targets.sort((a,b) => a.energy - b.energy);
-         if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-           creep.moveTo(targets[0]);
-         }else{
-             //console.log(creep.name + ' is repairing ' + targets[0].structureType);
-         }
-       }else{//if no building target
-
-         targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-         if(targets.length) {
-           if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-             creep.moveTo(targets[0]);
-           }
-         }else{//if no construction target
-           creep.moveTo(Memory.Idle);
-         }//END if no construction target
-
-       }//END if no building target
-
-     }//END if building
-
-   }//END run function
-
- };module.exports = roleBuilder;
+                        if(targets.length) {
+                          targets.sort((a,b) => a.hits - b.hits);
+                          if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(targets[0]);
+                          }
+                        }else{//if no building target
+                          targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                          if(targets.length) {
+                            if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                              creep.moveTo(targets[0]);
+                            }
+                          }else{//if no construction target
+                            roleUpgrader.run(creep);
+                          }//END if no construction target
+                        }//END if no building target
+                      }//END if building
+                    }//END about to die
+                  }//END kill creep // Memory.builder.length > Memory.builderTotal || (creep.memory.level < Memory.cityLevel && Memory.builderFull
+                }//END run function
+              }
