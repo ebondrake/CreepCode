e890b3f4403a8438124cfc0d82160b5be228dc92
ebondrake <ebon.drake@outlook.com>
2016-07-12 10:34:06 -0600

    Update role.attacker.js

--- role.attacker.js
+++ role.attacker.js
@@ -1,19 +1,45 @@
-var roleAttacker = {
+var roleHealer = require('role.healer');
 
+module.exports={
   run: function(creep) {
+    if(Memory.attacker.length > Memory.attackerTotal || (creep.memory.level < Memory.cityLevel && Memory.attacker)){
+      creep.drop(RESOURCE_ENERGY);
+      creep.moveTo(Game.spawns.Home);
+    }else{
+      if(creep.ticksToLive >= 1450 || Game.spawns.Home.energy < 20) {creep.memory.aboutToDie = false}
 
-    var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
-    if(target){
-      if(creep.attack(target) == ERR_NOT_IN_RANGE){
-        creep.moveTo(target);
-      }else {
-        console.log('Attacking: ' + target + '!');
-      }
-    }else {
-      creep.moveTo(Game.flags.Attacker);
+
+        var targetWall = Game.getObjectById('578108df356031b25fe69a1b');
+        var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+        if(targetWall){
+          //console.log(targetWall);
+          if(creep.dismantle(targetWall) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targetWall);
+          }
+        }else if(target){
+          if(creep.attack(target) == ERR_NOT_IN_RANGE){
+            creep.moveTo(target);
+          }
+        }else{
+          var target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_SPAWN);}});
+          if(!target){
+            var target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_EXTENSION);}});
+            if(!target){
+              var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType === STRUCTURE_WALL) && (structure.hits  < 300000);}});
+            }
+          }
+
+          if(target) {
+              if(creep.dismantle(target) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(target);
+              }
+          }else {
+            //roleHealer.run(creep);
+            creep.moveTo(Game.flags.KILL);
+          }
+        }
     }
-
-  }//end run function
-
-};module.exports = roleAttacker;
+  }
+}
