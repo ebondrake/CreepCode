61f7024c75cbcb2e348bfe865e474dbe7a600451
ebondrake <ebon.drake@outlook.com>
2016-07-12 10:35:27 -0600

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,49 +1,70 @@
-var roleHarvester = {
-  run: function(creep) {
+var roleCollector = require('role.collector');
 
-    //if harvesting value is null set to false.
-    if(creep.memory.harvesting === null){
-      creep.memory.harvesting = false;
-    }
-
-    //if not harvesting and not carrying energy set harvesting to true.
-    if(!creep.memory.harvesting && creep.carry.energy === 0){
-      //var energy = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-      creep.memory.harvesting = true;
-    }
-
-    //if harvesting and carrying full energy set harvesting to false.
-    if(creep.memory.harvesting && creep.carry.energy === creep.carryCapacity){
-      creep.memory.harvesting = false;
-    }
-
-    //if harvesting then do this
-    if(creep.memory.harvesting){
-      if(creep.carry.energy < creep.carryCapacity){
-        var droppedEnergy = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
-        if(droppedEnergy){
-          if(creep.pickup(droppedEnergy) === ERR_NOT_IN_RANGE){
-            creep.moveTo(droppedEnergy);
-          }
-        }else{
-          var energy = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-          if(creep.harvest(energy) === ERR_NOT_IN_RANGE){
-            creep.moveTo(energy);
-          }
-        }
-      }
-      //if not harvesting then to this
+module.exports={
+  run: function(creep){
+    if(Memory.harvester.length > Memory.harvesterTotal || (creep.memory.level < Memory.cityLevel && Memory.harveste.length > 1)){
+      creep.drop(RESOURCE_ENERGY);
+      creep.moveTo(Game.spawns.Home);
     }else{
-      if(creep.room.energyAvailable < creep.room.energyCapacityAvailable){
-        var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_TOWER || structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;}});
-        if(creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-          if(creep.moveTo(target) === ERR_NOT_FOUND){
-            creep.moveTo(Game.flags.Idle);
+
+
+    if(creep.ticksToLive >= 1450 || Game.spawns.Home.energy < 20) {creep.memory.aboutToDie = false}
+
+    if(!creep.memory.working && creep.carry.energy === 0) {creep.memory.working = true;}
+    if(creep.memory.working && creep.carry.energy === creep.carryCapacity){
+      if(Memory.collector.length > 0){
+        if (creep.room.name === Memory.Home.name){creep.drop(RESOURCE_ENERGY)};
+      }else {
+        creep.memory.working = false;
+      }
+    }
+
+    if(creep.memory.aboutToDie){
+      creep.drop(RESOURCE_ENERGY);
+      creep.moveTo(Game.spawns.Home);
+    }else {
+      if(creep.memory.working){
+        if(creep.carry.energy < creep.carryCapacity){
+
+          var droppedEnergy = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+          if(droppedEnergy && Memory.collector.length === 0){
+            if(creep.pickup(droppedEnergy) === ERR_NOT_IN_RANGE){
+              creep.moveTo(droppedEnergy);
+            }
+          }else{
+            var energy = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            if(energy){
+              if(creep.harvest(energy) === ERR_NOT_IN_RANGE){
+                creep.moveTo(energy);
+              }
+            }else{
+                energySource = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+                if(energySource){
+                    if(creep.pickup(energySource) === ERR_NOT_IN_RANGE){creep.moveTo(energySource);}
+                    if (creep.room.name === Memory.Room01.name){creep.moveTo(Memory.Idle);}
+                    
+                }
+              //if(creep.room.name === Memory.Home.name){creep.moveTo(Game.flags.KILL);}
+              
+              //creep.drop(RESOURCE_ENERGY);
+              //creep.moveTo(Memory.Idle);
+              //roleCollector.run(creep);
+          
+            }
           }
         }
       }else{
-        creep.moveTo(Memory.Idle);
+        //creep.moveTo(Memory.Idle);
+        if(Memory.collector.length === 0){roleCollector.run(creep);}else {
+          creep.drop(RESOURCE_ENERGY);
+          creep.memory.working = true;
+
+        }
+        //else {roleBuilder.run(creep);}
       }
     }
-  }//END run function
-};module.exports = roleHarvester;
+
+}
+
+  }
+}
